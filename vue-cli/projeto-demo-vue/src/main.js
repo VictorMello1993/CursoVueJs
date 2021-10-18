958c4c526d1c5a6d020229c0a84d57e83fb0b240
Victor Mello <victorsmello93@gmail.com>
2021-10-18 00:53:07 -0300

    Aula adicionando plugins ao projeto Vue

--- vue-cli/projeto-demo-vue/src/main.js
+++ vue-cli/projeto-demo-vue/src/main.js
@@ -3,12 +3,15 @@
 
 import Vue from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify'
 
 Vue.config.productionTip = false
 
 new Vue({
+  vuetify,
+
   //el: #app; 
   //Ou utilizando $mount() como está na linha abaixo
 
-  render: h => h(App),
+  render: h => h(App)
 }).$mount('#app')
