ef4dfa7e79deab28b8504fb52ce2018078c8ca4f
Victor Mello <victorsmello93@gmail.com>
2021-10-27 07:44:23 -0300

    Ajustando os imports de componentes num caminho absoluto

---
+++ cap4-componentes/componente-desafio/src/views/Home/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app>    
+    <app-nav />
+    <app-content/>
+    <app-caroulsel/>    
+    <app-footer/>    
+  </v-app>
+</template>
+
+<script>
+import Navigation from "@/components/Navigation/NavigationDrawer.vue";
+import Content from '@/components/Content/Content.vue';
+import Carousel from '@/components/Carousel/AppCarousel.vue';
+import Footer from '@/components/Footer/Footer.vue';
+
+export default {
+  components: {
+    'app-nav': Navigation,
+    'app-content': Content,
+    'app-caroulsel': Carousel,
+    'app-footer': Footer
+  },  
+};
+</script>
