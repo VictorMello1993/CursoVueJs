ceef3718b780a6da303b62e30176a0597266f313
Victor Mello <victorsmello93@gmail.com>
2021-10-27 22:58:28 -0300

    Comunicação direta entre componentes e validação das propriedades dos componentes

--- cap5-comunicacao-componentes/projeto-componentes-comunicacao/src/components/Usuario.vue
+++ cap5-comunicacao-componentes/projeto-componentes-comunicacao/src/components/Usuario.vue
@@ -2,10 +2,23 @@
     <div class="container">
         <h1>Componente Usuário</h1>
         <p>Esse é um componente muito legal!</p>
-        <button @click="alterNome">Alterar nome</button>
+        <button @click="alterarNome">Alterar nome</button>
         <hr>
         <div class="componentes">
-            <app-usuario-info />
+            <!-- Uma das formas de comunicação entre os componentes: usando propriedades -->
+            <!-- No caso, foi criada a propriedade "nome" passando o valor "Pedro" -->
+            <!-- E o componente filho (no caso, app-usuario-info) precisa receber essa propriedade
+                 para interpolar o valor que foi passado do componente pai (Usuario)
+             -->
+             <!-- Porém, esse valor é estático, não foi alterado para o valor que foi trocado a partir do atributo
+                   'data' que foi passado o nome 'Victor'. Para resolver o valor de uma propriedade, basta utilizar
+                   a diretiva v-bind:<propriedade> -->
+
+            <!--OBS: definir o nome das propriedades dos componentes é case sensitive! Ou seja, embora no HTML 
+                     as tags sejam case insensitive, no momento de compilar o que foi definido no componente para o 
+                     código Javascript, haverá uma diferença entre letras maiúsculas e minúsculas, pois utiliza 
+                     o Camel Case na definição de identificadores de variáveis e funções.  -->
+            <app-usuario-info :nome="nome"/>
             <app-usuario-editar />
         </div>
     </div>
@@ -24,7 +37,7 @@ export default {
     },
 
     methods: {
-        alterNome(){
+        alterarNome(){
             this.nome = 'Pedro'
         }
     }
--- cap5-comunicacao-componentes/projeto-componentes-comunicacao/src/components/UsuarioInfo.vue
+++ cap5-comunicacao-componentes/projeto-componentes-comunicacao/src/components/UsuarioInfo.vue
@@ -1,21 +1,49 @@
 <template>
-    <div class="componente">
-        <h2>As Informações de Usuário</h2>
-        <p>Vários detalhes...</p>
-        <p>Nome do usuário: <strong>{{nome}}</strong></p>
-    </div>
+  <div class="componente">
+    <h2>As Informações de Usuário</h2>
+    <p>Vários detalhes...</p>
+    <p>
+      Nome do usuário: <strong>{{ inverterNome() }}</strong>
+    </p>
+  </div>
 </template>
 
 <script>
 export default {
-    
-}
+  //Recebendo a propriedade 'nome' que foi passada do componente pai (Usuario.vue)
+  // props: ['nome'],
+
+  //Validando propriedades
+  props: {
+    nome: {
+      type: String,
+    //   required: true,
+      default: "Anônimo",
+    },
+  },
+
+  methods: {
+    inverterNome() {
+      return this.nome.split("").reverse().join("");
+    },
+  },
+
+  //CONFLITO! Já existe um atributo que na verdade é uma propriedade 'nome' definido no props
+  // data(){
+  //     return {
+  //         nome: ''
+  //     }
+  // }
+
+  /*OBS: uma propriedade que é recebida pelo componente filho pode ser tratado como dado que foi
+    definido no atributo data, computed, watch e até em methods*/
+};
 </script>
 
 <style scoped>
-    .componente {
-        flex: 1;
-        background-color: #ec485f;
-        color: #fff;
-    }
+.componente {
+  flex: 1;
+  background-color: #ec485f;
+  color: #fff;
+}
 </style>
